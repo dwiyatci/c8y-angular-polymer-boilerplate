22755c00f2c8c53b2023d64ab91696cd5174be52
Glenn Dwiyatcita <dwiyatci@mail.com>
2018-08-28 17:47:26 +0200

    ✅ test: add test helpers

---
+++ src/testing/router-directive-link-stub.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+import { Directive, Input } from '@angular/core';
+
+@Directive({
+  selector: '[routerLink]',
+  host: { '(click)': 'onClick()' }
+})
+export class RouterLinkDirectiveStub {
+  @Input('routerLink') linkParams: any;
+  navigatedTo: any = null;
+
+  onClick() {
+    this.navigatedTo = this.linkParams;
+  }
+}
