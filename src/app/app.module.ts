a5a6e6d32103f26454e4090fa2588b11bed4afca
Glenn Dwiyatcita <dwiyatci@mail.com>
2018-10-14 19:17:12 +0200

    ✨ feat(@c8y packages): upgrade packages (esp. the latest @c8y/ngx-components version) and adapt the base code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, LOCALE_ID, NgModule } from '@angular/core';
 // import { OutletComponent } from '@c8y/ngx-components';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -16,6 +16,11 @@ import { SharedModule } from './shared/shared.module';
     LoginModule,
     AppRoutingModule
   ],
+  /**
+   * TODO: Remove this workaround after bug in cumulocity-ui/packages/ngx-components/core/i18n/locale.provider.ts is fixed, i.e.
+   * the value used for `LOCALE_ID` token (`localStorage.getItem('c8y_language')`) maybe null.
+   */
+  providers: [{ provide: LOCALE_ID, useValue: 'en-US' }],
   declarations: [
     AppComponent
   ],
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,18 +1,18 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
-import { C8yDataModule } from '@c8y/ngx-data';
+import { CommonModule } from '@c8y/ngx-components';
 
 import { throwIfAlreadyLoaded } from './module-import-guard';
 import { SelectivePreloadingStrategy } from './selective-preloading-strategy';
 
 @NgModule({
   imports: [
-    C8yDataModule
+    CommonModule
   ],
   providers: [
     SelectivePreloadingStrategy
   ],
   exports: [
-    C8yDataModule
+    CommonModule
   ]
 })
 export class CoreModule {
